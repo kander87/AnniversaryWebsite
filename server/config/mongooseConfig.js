4f292ee9b38bf08b2ffdf685db6678ad70a24879
kander87 <kaanders00@gmail.com>
2023-10-19 17:25:18 -0400

    adding atlas mongdb in attempt to make vercel app work

--- server/config/mongooseConfig.js
+++ server/config/mongooseConfig.js
@@ -1,8 +1,22 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/anniversarydb', {//change database name here
+// mongoose.connect('mongodb://127.0.0.1:27017/anniversarydb', {//change database name here
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+//     .then(() => console.log('Established a connection to the database'))
+//     .catch(err => console.log('Something went wrong when connecting to the database ', err));
+
+const mongoose = require("mongoose");
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose
+  .connect(DATABASE_URL, {
+    //change database name here
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Established a connection to the database'))
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Established a connection to the database"))
+  .catch((err) =>
+    console.log("Something went wrong when connecting to the database ", err)
+  );
